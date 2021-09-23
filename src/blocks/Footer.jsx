f35000197757994e8918b039fcbd12d708171e16
Derek Linan <4261674+DerekLinan@users.noreply.github.com>
2021-09-23 16:41:38 -0600

    Add Header and Footer, implement nav links

--- src/blocks/Footer.jsx
+++ src/blocks/Footer.jsx
@@ -1,5 +1,14 @@
+import '../styles/footer.scss';
+
 const Footer = () => {
-  return <h2>Footer</h2>;
+  return (
+    <footer className="footer">
+      <p className="footer__text">
+        Copyright <time dateTime="2021">2021</time> ya boi
+      </p>
+      <hr className="footer__horizontal-line" />
+    </footer>
+  );
 };
 
 export default Footer;
