91148cd549b5e2629e2e2a5a0d236f6be65af46a
Derek Linan <4261674+DerekLinan@users.noreply.github.com>
2021-10-06 13:46:29 -0600

    Remove React fragment from BookDetails.jsx

--- src/pages/BookDetails.jsx
+++ src/pages/BookDetails.jsx
@@ -20,40 +20,38 @@ const BookDetails = () => {
   const { title, image, author, rating, synopsis, pages, published } = book;
 
   return (
-    <>
-      <main className="main">
-        <section className="section">
-          <div className="book-details">
-            <div className="book-card">
-              <img
-                className="book-card__img"
-                crossOrigin="anonymous"
-                src={image}
-                alt={`${title}'s cover`}
-              />
-              <div className="label-before">
-                <StarRating rating={rating} />
-              </div>
-            </div>
-            <div className="book-details__info">
-              <h1 className="section__header">{title}</h1>
-              <h2 className="section__h2">{author}</h2>
-              <h3 className="section__h3">Published: {published}</h3>
-              <h3 className="section__h3">{pages} pages</h3>
-              <p className="section__text">{synopsis}</p>
+    <main className="main">
+      <section className="section">
+        <div className="book-details">
+          <div className="book-card">
+            <img
+              className="book-card__img"
+              crossOrigin="anonymous"
+              src={image}
+              alt={`${title}'s cover`}
+            />
+            <div className="label-before">
+              <StarRating rating={rating} />
             </div>
           </div>
-          <div className="button-group">
-            <button className="button" type="button">
-              Edit This Book
-            </button>
-            <button className="button button--secondary-color" type="button">
-              Back to Shelf
-            </button>
+          <div className="book-details__info">
+            <h1 className="section__header">{title}</h1>
+            <h2 className="section__h2">{author}</h2>
+            <h3 className="section__h3">Published: {published}</h3>
+            <h3 className="section__h3">{pages} pages</h3>
+            <p className="section__text">{synopsis}</p>
           </div>
-        </section>
-      </main>
-    </>
+        </div>
+        <div className="button-group">
+          <button className="button" type="button">
+            Edit This Book
+          </button>
+          <button className="button button--secondary-color" type="button">
+            Back to Shelf
+          </button>
+        </div>
+      </section>
+    </main>
   );
 };
 
