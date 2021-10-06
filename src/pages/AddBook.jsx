633a8ceeb305c7667c0e527c4e5429b17e9d64f9
Derek Linan <4261674+DerekLinan@users.noreply.github.com>
2021-10-06 13:43:57 -0600

    Remove react fragments in AddBook.jsx

--- src/pages/AddBook.jsx
+++ src/pages/AddBook.jsx
@@ -25,99 +25,97 @@ const AddBook = () => {
   };
 
   return (
-    <>
-      <main className="main">
-        <section className="section">
-          <h1 className="section__header">Add Book</h1>
+    <main className="main">
+      <section className="section">
+        <h1 className="section__header">Add Book</h1>
 
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              addBook(bookData)
-                .then(() => {
-                  history.push('/bookshelf');
-                })
-                .catch((error) => {
-                  throw new Error(error);
-                });
-            }}
-          >
-            <div className="layout-block">
-              <p className="section__text add-title">Title</p>
-              <input
-                className="input__field add-title-field"
-                type="text"
-                value={title}
-                onChange={(e) => dataChanged('title', e)}
-                required
-                minLength="1"
-                maxLength="100"
-              />
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            addBook(bookData)
+              .then(() => {
+                history.push('/bookshelf');
+              })
+              .catch((error) => {
+                throw new Error(error);
+              });
+          }}
+        >
+          <div className="layout-block">
+            <p className="section__text add-title">Title</p>
+            <input
+              className="input__field add-title-field"
+              type="text"
+              value={title}
+              onChange={(e) => dataChanged('title', e)}
+              required
+              minLength="1"
+              maxLength="100"
+            />
 
-              <p className="section__text add-author">Author</p>
-              <input
-                className="input__field add-author-field"
-                type="text"
-                value={author}
-                onChange={(e) => dataChanged('author', e)}
-                required
-                minLength="1"
-                maxLength="100"
-              />
+            <p className="section__text add-author">Author</p>
+            <input
+              className="input__field add-author-field"
+              type="text"
+              value={author}
+              onChange={(e) => dataChanged('author', e)}
+              required
+              minLength="1"
+              maxLength="100"
+            />
 
-              <div className="book-cover">
-                <img className="book-cover__image" src={image} alt="cover" />
-                <p className="book-cover__text">Add Image</p>
-              </div>
-              <button className="button grid--reposition" type="button">
-                Add Image
-              </button>
-
-              <p className="section__text add-synopsis">Synopsis</p>
-              <textarea
-                className="input__area add-synopsis-field"
-                type="text"
-                value={synopsis}
-                onChange={(e) => dataChanged('synopsis', e)}
-                rows="16"
-              />
-
-              <p className="section__text add-published">Published</p>
-              <input
-                type="date"
-                value={published}
-                onChange={(e) => dataChanged('published', e)}
-                className="input__field add-published-field"
-              />
-
-              <p className="section__text add-pages">Pages</p>
-              <input
-                className="input__field add-pages-field"
-                type="number"
-                value={pages}
-                onChange={(e) => dataChanged('pages', e)}
-              />
-
-              <p className="section__text add-rating">Rating</p>
-              <StarRating
-                onChange={(newRating) => dataChanged('rating', newRating)}
-                value={rating}
-                rating={rating}
-              />
+            <div className="book-cover">
+              <img className="book-cover__image" src={image} alt="cover" />
+              <p className="book-cover__text">Add Image</p>
             </div>
+            <button className="button grid--reposition" type="button">
+              Add Image
+            </button>
 
-            <div className="button-group">
-              <button className="button" type="submit">
-                Add Book
-              </button>
-              <button className="button button--secondary-color" type="button">
-                Cancel
-              </button>
-            </div>
-          </form>
-        </section>
-      </main>
-    </>
+            <p className="section__text add-synopsis">Synopsis</p>
+            <textarea
+              className="input__area add-synopsis-field"
+              type="text"
+              value={synopsis}
+              onChange={(e) => dataChanged('synopsis', e)}
+              rows="16"
+            />
+
+            <p className="section__text add-published">Published</p>
+            <input
+              type="date"
+              value={published}
+              onChange={(e) => dataChanged('published', e)}
+              className="input__field add-published-field"
+            />
+
+            <p className="section__text add-pages">Pages</p>
+            <input
+              className="input__field add-pages-field"
+              type="number"
+              value={pages}
+              onChange={(e) => dataChanged('pages', e)}
+            />
+
+            <p className="section__text add-rating">Rating</p>
+            <StarRating
+              onChange={(newRating) => dataChanged('rating', newRating)}
+              value={rating}
+              rating={rating}
+            />
+          </div>
+
+          <div className="button-group">
+            <button className="button" type="submit">
+              Add Book
+            </button>
+            <button className="button button--secondary-color" type="button">
+              Cancel
+            </button>
+          </div>
+        </form>
+      </section>
+    </main>
   );
 };
 
