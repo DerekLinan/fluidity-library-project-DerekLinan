e8ed42cab5731ba2633b2ad9c3375e7813fd4b44
Derek Linan <4261674+DerekLinan@users.noreply.github.com>
2021-10-11 19:39:03 -0600

    Make StarRating DRY, Align star input

--- src/blocks/StarRating.jsx
+++ src/blocks/StarRating.jsx
@@ -38,158 +38,39 @@ const StarRating = ({ rating, onChange }) => {
     }
   };
 
+  const starDisplay = (star) => {
+    const pos = ['left', 'right'];
+    return (
+      <div className={`rating__display-${star}`}>
+        {display(star, displayRating || currentRating)}
+        {pos.map((text, index) => (
+          <label
+            className={`rating__label rating__${text}`}
+            htmlFor={`rating-${star}`}
+          >
+            <input
+              className="rating__input"
+              type="radio"
+              name="rating"
+              id={`rating-${star * 2 - (1 - index)}`}
+              value={star - 0.5 * (1 - index)}
+              checked={currentRating === star - 0.5 * (1 - index)}
+              onChange={ratingChanged}
+              onMouseEnter={() => changeDisplay(star - 0.5 * (1 - index))}
+              onMouseLeave={() => changeDisplay(0)}
+            />
+          </label>
+        ))}
+      </div>
+    );
+  };
+
+  const stars = [1, 2, 3, 4, 5];
+
   return (
     <div className="add-rating-field">
       <div className="rating" form="rating">
-        <div className="rating__display-1">
-          {display(1, displayRating || currentRating)}
-          <label className="rating__label rating__left" htmlFor="rating-1">
-            <input
-              className="rating__input"
-              type="radio"
-              name="rating"
-              id="rating-1"
-              value="0.5"
-              checked={currentRating === '0.5'}
-              onChange={ratingChanged}
-              onMouseEnter={() => changeDisplay(0.5)}
-              onMouseLeave={() => changeDisplay(0)}
-            />
-          </label>
-          <label className="rating__label rating__right" htmlFor="rating-2">
-            <input
-              className="rating__input"
-              type="radio"
-              name="rating"
-              id="rating-2"
-              value="1"
-              checked={currentRating === '1'}
-              onChange={ratingChanged}
-              onMouseEnter={() => changeDisplay(1)}
-              onMouseLeave={() => changeDisplay(0)}
-            />
-          </label>
-        </div>
-
-        <div className="rating__display-2">
-          {display(2, displayRating || currentRating)}
-          <label className="rating__label rating__left" htmlFor="rating">
-            <input
-              className="rating__input"
-              type="radio"
-              name="rating"
-              id="rating-3"
-              value="1.5"
-              checked={currentRating === '1.5'}
-              onChange={ratingChanged}
-              onMouseEnter={() => changeDisplay(1.5)}
-              onMouseLeave={() => changeDisplay(0)}
-            />
-          </label>
-          <label className="rating__label rating__right" htmlFor="rating">
-            <input
-              className="rating__input"
-              type="radio"
-              name="rating"
-              id="rating-4"
-              value="2"
-              checked={currentRating === '2'}
-              onChange={ratingChanged}
-              onMouseEnter={() => changeDisplay(2)}
-              onMouseLeave={() => changeDisplay(0)}
-            />
-          </label>
-        </div>
-
-        <div className="rating__display-3">
-          {display(3, displayRating || currentRating)}
-          <label className="rating__label rating__left" htmlFor="rating">
-            <input
-              className="rating__input"
-              type="radio"
-              name="rating"
-              id="rating-5"
-              value="2.5"
-              checked={currentRating === '2.5'}
-              onChange={ratingChanged}
-              onMouseEnter={() => changeDisplay(2.5)}
-              onMouseLeave={() => changeDisplay(0)}
-            />
-          </label>
-          <label className="rating__label rating__right" htmlFor="rating">
-            <input
-              className="rating__input"
-              type="radio"
-              name="rating"
-              id="rating-6"
-              value="3"
-              checked={currentRating === '3'}
-              onChange={ratingChanged}
-              onMouseEnter={() => changeDisplay(3)}
-              onMouseLeave={() => changeDisplay(0)}
-            />
-          </label>
-        </div>
-
-        <div className="rating__display-4">
-          {display(4, displayRating || currentRating)}
-          <label className="rating__label rating__left" htmlFor="rating">
-            <input
-              className="rating__input"
-              type="radio"
-              name="rating"
-              id="rating-7"
-              value="3.5"
-              checked={currentRating === '3.5'}
-              onChange={ratingChanged}
-              onMouseEnter={() => changeDisplay(3.5)}
-              onMouseLeave={() => changeDisplay(0)}
-            />
-          </label>
-          <label className="rating__label rating__right" htmlFor="rating">
-            <input
-              className="rating__input"
-              type="radio"
-              name="rating"
-              id="rating-8"
-              value="4"
-              checked={currentRating === '4'}
-              onChange={ratingChanged}
-              onMouseEnter={() => changeDisplay(4)}
-              onMouseLeave={() => changeDisplay(0)}
-            />
-          </label>
-        </div>
-
-        <div className="rating__display-5">
-          {display(5, displayRating || currentRating)}
-          <label className="rating__label rating__left" htmlFor="rating">
-            <input
-              className="rating__input"
-              type="radio"
-              name="rating"
-              id="rating-9"
-              value="4.5"
-              checked={currentRating === '4.5'}
-              onChange={ratingChanged}
-              onMouseEnter={() => changeDisplay(4.5)}
-              onMouseLeave={() => changeDisplay(0)}
-            />
-          </label>
-          <label className="rating__label rating__right" htmlFor="rating">
-            <input
-              className="rating__input"
-              type="radio"
-              name="rating"
-              id="rating-10"
-              value="5"
-              checked={currentRating === '5'}
-              onChange={ratingChanged}
-              onMouseEnter={() => changeDisplay(5)}
-              onMouseLeave={() => changeDisplay(0)}
-            />
-          </label>
-        </div>
+        {stars.map((star) => starDisplay(star))}
       </div>
     </div>
   );
