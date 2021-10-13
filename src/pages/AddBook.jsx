be0fe77f16eb6d80ec357dafaaadcdb15152f627
Derek Linan <4261674+DerekLinan@users.noreply.github.com>
2021-10-13 16:11:42 -0600

    Add EditBook functionality

--- src/pages/AddBook.jsx
+++ src/pages/AddBook.jsx
@@ -1,39 +1,11 @@
-import '../styles/add-book.scss';
 import { useHistory } from 'react-router-dom';
-import { useState } from 'react';
-import StarRating from '../blocks/StarRating';
 import { addBook } from '../scripts/Utils';
-import noCover from '../images/no-cover.png';
+import BookForm from '../blocks/BookForm';
 
 const AddBook = () => {
   const history = useHistory();
-  const [bookData, setBookData] = useState({
-    title: '',
-    author: '',
-    image: '',
-    synopsis: '',
-    published: '',
-    pages: 0,
-    rating: 0,
-  });
-
-  const { title, author, image, synopsis, published, pages, rating } = bookData;
-
-  const dataChanged = (key, event) => {
-    if (typeof key === 'string') {
-      setBookData((prevData) => ({ ...prevData, [key]: event.target.value }));
-    }
-  };
-
-  const handleOnSubmit = (e) => {
-    e.preventDefault();
-    const titleInput = bookData.title.trim();
-    const authorInput = bookData.author.trim();
-
-    if (!(titleInput && authorInput))
-      return alert('Must include at least a title and author to add book');
-
-    addBook(bookData)
+  const addBookData = (data) => {
+    addBook(data)
       .then(() => {
         history.push('/bookshelf');
       })
@@ -42,96 +14,7 @@ const AddBook = () => {
       });
   };
 
-  return (
-    <main className="main">
-      <section className="section">
-        <h1 className="section__header">Add Book</h1>
-
-        <form onSubmit={(e) => handleOnSubmit(e)}>
-          <div className="layout-block">
-            <p className="section__text add-title">Title</p>
-            <input
-              className="input__field add-title-field"
-              type="text"
-              value={title}
-              onChange={(e) => dataChanged('title', e)}
-              required
-              minLength="1"
-              maxLength="100"
-            />
-
-            <p className="section__text add-author">Author</p>
-            <input
-              className="input__field add-author-field"
-              type="text"
-              value={author}
-              onChange={(e) => dataChanged('author', e)}
-              required
-              minLength="1"
-              maxLength="100"
-            />
-
-            <div className="book-cover">
-              <img
-                className="book-cover__image"
-                src={image || noCover}
-                alt="cover"
-              />
-              <p className="book-cover__text">Add Image</p>
-            </div>
-            <button className="button grid--reposition" type="button">
-              Add Image
-            </button>
-
-            <p className="section__text add-synopsis">Synopsis</p>
-            <textarea
-              className="input__area add-synopsis-field"
-              type="text"
-              value={synopsis}
-              onChange={(e) => dataChanged('synopsis', e)}
-              rows="16"
-            />
-
-            <p className="section__text add-published">Published</p>
-            <input
-              type="date"
-              value={published}
-              onChange={(e) => dataChanged('published', e)}
-              className="input__field add-published-field"
-            />
-
-            <p className="section__text add-pages">Pages</p>
-            <input
-              className="input__field add-pages-field"
-              type="number"
-              value={pages}
-              onChange={(e) => dataChanged('pages', e)}
-            />
-
-            <p className="section__text add-rating">Rating</p>
-            <StarRating
-              onChange={(newRating) => dataChanged('rating', newRating)}
-              value={rating}
-              rating={rating}
-            />
-          </div>
-
-          <div className="button-group">
-            <button className="button" type="submit">
-              Add Book
-            </button>
-            <button
-              className="button button--secondary-color"
-              type="button"
-              onClick={() => history.push('/bookshelf')}
-            >
-              Cancel
-            </button>
-          </div>
-        </form>
-      </section>
-    </main>
-  );
+  return <BookForm callBackFunc={(data) => addBookData(data)} />;
 };
 
 export default AddBook;
--- src/pages/EditBook.jsx
+++ src/pages/EditBook.jsx
@@ -1,56 +1,21 @@
-import StarRating from '../blocks/StarRating';
-import noCover from '../images/no-cover.png';
+import { useHistory, useParams } from 'react-router-dom';
+import BookForm from '../blocks/BookForm';
+import { editBook } from '../scripts/Utils';
 
 const EditBook = () => {
-  return (
-    <>
-      <main className="main">
-        <section className="section">
-          <h1 className="section__header">Add Book</h1>
+  const history = useHistory();
+  const { id } = useParams();
+  const editBookData = (data) => {
+    editBook(id, data)
+      .then(() => {
+        history.push('/bookshelf');
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
+  };
 
-          <div className="layout-block">
-            <label className="section__text add-title">Title</label>
-            <input
-              className="input__field add-title-field"
-              type="text"
-              autoFocus
-            />
-
-            <label className="section__text add-author">Author</label>
-            <input className="input__field add-author-field" type="text" />
-
-            <div className="book-cover">
-              <img className="book-cover__image" src={noCover} />
-              <p className="book-cover__text">Add Image</p>
-            </div>
-            <button className="button grid--reposition">Add Image</button>
-
-            <label className="section__text add-synopsis">Synopsis</label>
-            <textarea
-              className="input__area add-synopsis-field"
-              type="text"
-              rows="16"
-            />
-
-            <label className="section__text add-published">Published</label>
-            <input type="date" className="input__field add-published-field" />
-
-            <label className="section__text add-pages">Pages</label>
-            <input className="input__field add-pages-field" type="number" />
-
-            <label className="section__text add-rating">Rating</label>
-            <StarRating />
-          </div>
-
-          <div className="button-group">
-            <button className="button">Submit</button>
-            <button className="button button--secondary-color">Cancel</button>
-            <button className="button button--danger-color">Delete Book</button>
-          </div>
-        </section>
-      </main>
-    </>
-  );
+  return <BookForm callBackFunc={(data) => editBookData(data)} />;
 };
 
 export default EditBook;
