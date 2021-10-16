b5182128a0703e6b706a4d2b2cb979211e9ce6b5
Derek Linan <4261674+DerekLinan@users.noreply.github.com>
2021-10-15 18:09:10 -0600

    Fix .js formating

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 const corsOptions = {
-    origin: 'http://localhost:1234',
+  origin: 'http://localhost:1234',
 };
 
 app.use(logger('dev'));
@@ -13,14 +13,14 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('../client/dist'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../client/dist'));
 }
 
 app.get('/', (req, res) => {
-    console.log("test")
-    res.sendStatus(200)
-})
+  console.log('test');
+  res.sendStatus(200);
+});
 app.listen(PORT, () => {
-    console.log(`API server listening on http://localhost:${PORT}!`)
-})
+  console.log(`API server listening on http://localhost:${PORT}!`);
+});
