3a64c1a69d207308ea4b7214821d5fc778f0739e
Derek Linan <4261674+DerekLinan@users.noreply.github.com>
2021-10-18 14:42:15 -0600

    Add author, book sequelize models

---
+++ server/models/author.js
@@ -0,0 +1,28 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Author extends Model {
+    static associate({ Book }) {
+      this.belongsToMany(Book);
+    }
+  }
+  Author.init(
+    {
+      first_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        require: true,
+        validate: {
+          len: [1, 255],
+        },
+      },
+      last_name: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      tableName: 'Authors',
+      modelName: 'Author',
+    }
+  );
+  return Author;
+};
